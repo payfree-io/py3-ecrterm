f853a6a93c6e86df9eb2995076f0d731122da32a
Paweł Konik <pawel.konik@wenzel-consulting.io>
2021-10-26 09:55:14 +0200

    increasing sleep time for cancellation

--- ecrterm/ecr.py
+++ ecrterm/ecr.py
@@ -345,7 +345,7 @@ class ECR(object):
         if self.transport.insert_delays:
             # we actually make a small sleep, allowing better flow.
             sleep(0.2)
-        sleep(1)
+        sleep(4)
         transmission = self.transmitter.abort(AbortCommand(), [])
         return transmission
 
